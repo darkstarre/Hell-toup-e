16fd1a36d28f872970e7f36e9b6f4a532f3719ee
Megan Nilles <darkstarre@hotmail.com>
2016-04-11 12:47:46 -0500

    more tests

--- test/integration/user_can_see_subjects_basic_info.rb
+++ test/integration/user_can_see_subjects_basic_info.rb
@@ -1,6 +1,20 @@
+require 'test_helper'
+
 class UserCanSeeSubjectsBasicInfo < ActionDispatch::IntegrationTest
   #user can see the subjects basic information 
-  #user can click links to tweets replies and media
+  #user can click links to tweets, replies, and media
   #user can choose to follow subject
-  #user can tweet them
+  #user can tweet the subject
+
+  #===================Navigation========================
+  page.visit '/homepage'
+  page.current_path 
+
+  # ==================Clicking links/buttons============
+  body = ""
+  page.visit '/homepage'
+
+  #click link by id
+  page.click_on 'tweets'
+  page.current_path
 end 
