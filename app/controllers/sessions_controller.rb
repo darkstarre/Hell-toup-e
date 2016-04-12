eb27068d244e9d5c7eb319d9549b92dbae167bfe
derreck <mansheim23@hotmail.com>
2016-04-11 20:05:31 -0500

    Weeeeeeee made it work.  Really, work.  Yuge changes ahead

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,19 @@
 class SessionsController < ApplicationController
   skip_before_filter :verify_authenticity_token, :only => :create
+
   def welcome
   end
 
   def create
+    auth= request.env["omniauth.auth"]
+    user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
+    session[:user_id] = user.id
+    redirect_to '/', :notice => "Signed in!"
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to '/', :notice => "Signed out!"
   end
 
   def auth_hash
--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,25 @@
 Rails.application.routes.draw do
+  get '/' => 'welcome#index'
   get '/auth/twitter', :as => 'twitter_auth'
-  match '/auth/twitter/callback' => 'sessions#create', :via => [:get, :post]
-
-  
-
+  match "/auth/:provider/callback" => "sessions#create", :via => [:get, :post]
+  get "/signout" => "sessions#destroy", :as => :signout
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'sessions#welcome'
-   
+
 
   # Example of regular route:
-    get 'sessions/welcome' => 'sessions#welcome'
-    post 'sessions/create' => 'sessions#create'
-    delete 'sessions/destroy' => 'sessions#destroy'
-    get 'users/show' => 'users#show'
-  
-    get '/tweets/' => 'tweets#index'
-     
-     
+  get 'sessions/welcome' => 'sessions#welcome'
+  post 'sessions/create' => 'sessions#create'
+  delete 'sessions/destroy' => 'sessions#destroy'
+  get 'users/show' => 'users#show'
 
-    # get 'replies/' => 'replies#index'
-    # get 'media/'   => 'media#index'
+  get '/tweets/' => 'tweets#index'
+
+
+
+  # get 'replies/' => 'replies#index'
+  # get 'media/'   => 'media#index'
 
 end
